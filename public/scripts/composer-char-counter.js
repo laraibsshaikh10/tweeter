a2685c37d3453a129bfcf40b6f5309258dd6cf4b
laraibsshaikh10 <laraibsshaikh10@gmail.com>
2024-03-20 17:43:06 -0400

    Added error message if character limit is exceeded

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,7 +4,10 @@ $(document).ready(function() {
  
     // --- our code goes here ---
   // console.log("i am ready.");
-  //.tweet-textbox textarea selects the text area, .on('input', function(){}) registers eventhandler for input elemetn
+
+  //assign the class counter to a variable to avoid querying the DOM multiple times
+  const $counter = $('.counter');
+  //.tweet-textbox textarea selects the text area, .on('input', function(){}) registers eventhandler for input element
   $('#tweet-text').on('input', function() {
     //to log text area element
     // console.log(this);
@@ -24,15 +27,22 @@ $(document).ready(function() {
     
     
     //to update the class counter in output based on the characters typed in the text area 
-    $('.counter').text(remainingCharacters);
+    $counter.text(remainingCharacters);
     
     //to see if input is invalid
     if (remainingCharacters < 0) {
       //add class to change counter color to red
-      $('.counter').addClass('invalid-input');
+      $counter.addClass('invalid-input');
+
+      //show error message
+      $('.error').text('Character limit has been exceeded.').show();
     } else {
       //remove class if input is valid and within range
-      $('.counter').removeClass('invalid-input')
+      $counter.removeClass('invalid-input');
+
+      //hide error if input is valid
+      $('.error').hide();
+
     }
     
   });
